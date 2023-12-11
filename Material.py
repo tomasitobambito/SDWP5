36f714e5b6b25edb64bac85a554a8ccc9d9ddf27
tomasitobambito <tomgacek0604@gmail.com>
2023-12-11 13:01:39 +0100

    added basic read/write for material catalogue

--- Material.py
+++ Material.py
@@ -1,4 +1,37 @@
+import os.path
+
 class Material:
-    def __init__(self, youngsModulus, poissonRatio):
-        self.youngsModulus = youngsModulus
-        self.poissonRatio = poissonRatio
+    def __init__(self, name, params):
+        self.name = name
+        self.youngsModulus = params[0]
+        self.poissonRatio = params[1]
+        self.density = params[2]
+
+class MaterialManager:
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            with open(path, 'x') as file:
+                pass
+
+        self.materials = []
+        self.read_file(path)
+
+    def read_file(self, path):
+        with open(path) as file:
+            for row in file:
+                if row:
+                    rowArray = row.split(';')
+
+                    currentMaterial = Material(rowArray[0], [float(param) for param in rowArray[1:]])
+
+                    self.materials.append(currentMaterial)
+
+    def write_catalouge(self, path):
+        with open(path, 'w') as file:
+            for material in self.materials:
+                currentLine = ";".join([material.name, 
+                    str(material.youngsModulus), 
+                    str(material.poissonRatio), 
+                    str(material.density)])
+                currentLine += '\n'
+                file.write(currentLine)
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from BucklingChecks import euler_buckling, shell_buckling
+from Material import MaterialManager, Material
 
-pressure = float(input('Please input the pressure inside of the tank'))
-length = float(input('Please input the length of the tank'))
-radius = float(input('Please input the radius of the tank'))
+# pressure = float(input('Please input the pressure inside of the tank'))
+# length = float(input('Please input the length of the tank'))
+# radius = float(input('Please input the radius of the tank'))
+
+manager = MaterialManager('./materials.txt')
+
+for material in manager.materials:
+    print(material.youngsModulus, material.name)
+
+test = Material('gold', [5.0, 5.0, 5.0])
+manager.materials.append(test)
+
+manager.write_catalouge('./materials.txt')
